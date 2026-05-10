94f35045b751942e55601fcf398a3147bb72b324
Organizacao Ujs <58025555-organizacaoujso@users.noreply.replit.com>
2026-05-10 00:52:58 +0000

    fix vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
-
-export default defineConfig({
-  plugins: [react()],
+export default {
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
   build: {
     outDir: "dist",
   },
-});
+};
